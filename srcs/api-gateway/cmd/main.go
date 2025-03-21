df495dfa631fc6e80b68a75994f9570a1be61513
oumar <oumarlam154@gmail.com>
2025-03-21 00:46:26 +0000

    Refactor api-gateway code

--- srcs/api-gateway/cmd/main.go
+++ srcs/api-gateway/cmd/main.go
@@ -13,37 +13,26 @@ import (
 )
 
 func main() {
-	// Load environment variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	// Load variables
-	port := ":"+os.Getenv("API_GATEWAY_PORT")
+	port := os.Getenv("API_GATEWAY_PORT")
 	inventoryAPIURL := os.Getenv("INVENTORY_API_URL")
 	rabbitMQURL := os.Getenv("RABBITMQ_URL")
 
-	// rabbitMQURL := "amqp://guest:guest@localhost:5672/" // RabbitMQ URL
-
-	// Initialize RabbitMQ connection
 	rabbitMQService, err := services.NewRabbitMQService(rabbitMQURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 	}
 	defer rabbitMQService.Close()
 
-	// Initialize router
 	router := mux.NewRouter()
 
-	// Inventory API proxy routes
 	router.PathPrefix("/api/movies").Handler(handlers.NewInventoryHandler(inventoryAPIURL))
-
-	// Billing API route
 	router.HandleFunc("/api/billing", handlers.NewBillingHandler(rabbitMQService)).Methods("POST")
 
-	// Start server
-	// port := ":3000"
 	fmt.Printf("Gateway API is running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(":" + port, router))
 }
